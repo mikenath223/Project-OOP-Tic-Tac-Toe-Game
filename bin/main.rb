7b540e7487dbe7d73d3951b715a84bbbebbe49ba
ChristianOtieno <cgao273@gmail.com>
2019-11-19 00:35:04 +0300

    ADD user engagement instructions to the GAME

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,30 @@
 #!/usr/bin/env ruby
-puts "Hello!"
+puts "Hello welcome to the tic-tactoe game!"
+print "First player's name: "
+first_player_name = gets.chomp
+
+print "Second player's name: "
+second_player_name = gets.chomp
+
+puts " "
+
+print "#{first_player_name}, Kindly chose between 'X' or '0':"
+first_player_choice = gets.chomp
+
+puts "You have picked #{first_player_choice}"
+
+puts "Exellent! Now it's your opponent's turn"
+
+puts " "
+
+print "#{second_player_name}, Kindly chose between 'X' or '0':"
+second_player_choice = gets.chomp
+
+puts "You have picked #{second_player_choice}"
+
+puts " "
+
+puts "These are both your moves on the board: "
+
+
+
