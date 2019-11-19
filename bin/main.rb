395375bc045058285a2b911dee75db5194cc1c1a
mike <michgoldennathan17@gmail.com>
2019-11-19 16:56:36 +0100

    [Update]Made changes to user_interface adding loop as advised

--- bin/main.rb
+++ bin/main.rb
@@ -8,6 +8,9 @@ When you choose to play either X or O you also have to choose on which position
 of the board you will place your choice. May the odds be ever in your favor :)"
 puts " "
 
+puts "Here is the board below where both your choices will be displayed on the board. "
+puts " "
+
 print "Input First player's name: "
 first_player_name = gets.chomp
 
@@ -21,11 +24,15 @@ puts " "
 
 #     print "#{first_player_name}, Kindly chose between 'X' or '0': "
 #     first_player_choice = gets.chomp
+#Check to confirm if right input, if not request for right input
+
 
 #     puts "You have picked #{first_player_choice}"
 #     print "#{first_player_name}, Kindly chose from position 1 to 9 on which position of 
 #     the board you wish to place your choice? "
 #     first_player_board_position = gets.chomp
+#Check to confirm if right input, if not request for right input
+
 #     puts "Nice #{first_player_name} has chosen to play #{first_player_choice} at 
 #     position #{first_player_board_position}. Now your choice is displayed on the board below. "
 
@@ -36,10 +43,14 @@ puts " "
 
 #     print "#{second_player_name}, Kindly chose between 'X' or '0': "
 #     second_player_choice = gets.chomp
+#Check to confirm if right input, if not request for right input
+
 #     puts "You have picked #{second_player_choice}"
 #     print "#{second_player_name}, Kindly chose between 1 to 9 on which position of 
 #     the board you wish to place your choice? "
 #     second_player_board_position = gets.chomp
+#Check to confirm if right input, if not request for right input
+
 #     puts "Nice #{second_player_name} has chosen to play #{second_player_choice} at 
 #     position #{second_player_board_position}." 
 
@@ -57,5 +68,5 @@ elsif o_wins
     puts "Oh no! #{second_player_name}, You Lose :("
     puts " "
 else
-    puts "It's a draw!"
+    puts "It's a tie!"
 end
