973a45ed855b07db41b0e861612960dd27ff461e
mike <michgoldennathan17@gmail.com>
2019-11-22 04:50:35 +0100

    [Update]add main.rb to fix  merge compatibility

---
+++ bin/main.rb
@@ -0,0 +1,97 @@
+#!/usr/bin/env ruby
+puts "Hello welcome to the tic-tactoe game!"
+puts " "
+
+puts "The rules..."
+puts "The board is arranged in squares boxes which start from number 1 to 9. 
+When you choose to play either X or O you also have to choose on which position 
+of the board you will place your choice. May the odds be ever in your favor :)"
+puts " "
+
+puts "Here is the board below where both your choices will be displayed on the board. "
+puts " "
+
+print "Input First player's name: "
+first_player_name = gets.chomp
+
+print "Input Second player's name: "
+second_player_name = gets.chomp
+
+puts " "
+
+game_on = true
+
+#initial instructions for player
+
+while game_on
+
+#each move
+print "#{first_player_name}, Kindly chose between 'X' or '0': "
+    first_player_choice = gets.chomp
+# Check to confirm if right input, if not request for right input
+
+
+    puts "You have picked #{first_player_choice}"
+    print "#{first_player_name}, Kindly chose from position 1 to 9 on which position of 
+    the board you wish to place your choice? "
+    first_player_board_position = gets.chomp
+# Check to confirm if right input, if not request for right input
+
+    puts "Nice #{first_player_name} has chosen to play #{first_player_choice} at 
+    position #{first_player_board_position}. Now your choice is displayed on the board below. "
+
+#Now board is displayed
+    #loop for each move
+  if winner_and_or_draw
+      game_on = false
+  end
+    next if !game_on
+
+    puts "Exellent! Now it's your opponent's turn"
+
+    puts " "
+
+    print "#{second_player_name}, Kindly chose between 'X' or '0': "
+    second_player_choice = gets.chomp
+# Check to confirm if right input, if not request for right input
+
+    puts "You have picked #{second_player_choice}"
+    print "#{second_player_name}, Kindly chose between 1 to 9 on which position of 
+    the board you wish to place your choice? "
+    second_player_board_position = gets.chomp
+# Check to confirm if right input, if not request for right input
+
+    puts "Nice #{second_player_name} has chosen to play #{second_player_choice} at 
+    position #{second_player_board_position}." 
+
+#Now board is displayed
+
+    puts "Here is the result of the board "
+    #loop for each move
+  if winner_and_or_draw
+      game_on = false
+      #breaks loop
+  end
+
+end
+
+
+
+if game_on == false
+
+puts "Here is the result of the final board "
+
+
+  if x_wins
+    puts " "
+    puts "Congratulations! #{first_player_name}, You WIN!"
+    puts "Oh no! #{first_player_name}, You Lose :("
+    puts " "
+  elsif o_wins
+    puts "Congratulations! #{second_player_name}, You WIN!"
+    puts "Oh no! #{second_player_name}, You Lose :("
+    puts " "
+  else
+    puts "It's a tie!"
+  end
+end
