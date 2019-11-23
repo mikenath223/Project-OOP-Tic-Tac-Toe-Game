8f4d9f7bc091b091f1dd7cce0862678c29976e37
ChristianOtieno <cgao273@gmail.com>
2019-11-23 16:49:32 +0300

    [TEST] ADD test to switch_pick

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -37,3 +37,22 @@ RSpec.describe TicTacToe::Player do
     end
   end
 end
+
+RSpec.describe TicTacToe::Game do
+  let(:game) { TicTacToe::Game.new }
+  let(:board) { [[1, 2, 3], [4, 5, 6], [7, 8, 9]] }
+  describe 'switch_pick' do
+    context 'Outputs ''X'' to the board as player 1''s move' do
+      it do
+        expect(game.switch_pick(1)).to eq('X')
+      end
+    end
+  end
+  describe 'switch_pick' do
+    context 'Outputs ''O'' to the board, as player 2''s move' do
+      it do
+        expect(game.switch_pick(2)).to eq('O')
+      end
+    end
+  end
+end
